0a37eba16797c138cc914a222db0b9e0ef589a27
Iven <1254392000@qq.com>
2026-05-19 21:01:04 +0800

    feat: 客户端更新用户名

--- client/client.go
+++ client/client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 type Client struct {
@@ -33,6 +35,12 @@ func NewClient(serverIp string, serverPort int) *Client {
 
 }
 
+// 客户端接收服务器消息
+func (client *Client) ReceiveMessage() {
+	// 接收服务器消息，打印标准输出到stdout
+	io.Copy(os.Stdout, client.Conn)
+}
+
 // 显示菜单
 func (client *Client) Menu() bool {
 	fmt.Println("1、群聊模式")
@@ -49,6 +57,20 @@ func (client *Client) Menu() bool {
 		fmt.Println("Invalid choice, please try again.")
 		return false
 	}
+}
+
+// 更新用户名
+func (client *Client) UpdateName() bool {
+	fmt.Println("Please input rename: ")
+	fmt.Scanln(&client.Name)
+	sendMsg := "rename|" + client.Name + "\n"
+	_, err := client.Conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("conn.Write err:", err)
+		return false
+	}
+
+	return true
 }
 
 // 主业务
@@ -70,7 +92,7 @@ func (client *Client) Run() {
 			break
 		case 3:
 			// 更新用户名
-			fmt.Println("更新用户名")
+			client.UpdateName()
 			break
 		case 0:
 			// 退出
@@ -96,6 +118,9 @@ func main() {
 		fmt.Println(">>>>>>>>failed connect to server......")
 		return
 	}
+
+	// 启动接收服务器消息的goroutine
+	go client.ReceiveMessage()
 
 	fmt.Println(">>>>>>>>success connect to server......")
 
